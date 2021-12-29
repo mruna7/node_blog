0567619a274016b96b1d701497219e73c3230eec
Riddhi Goyal <riddhi.goyal@impetus.com>
2021-12-29 11:26:21 +0530

    Modified serverless.yml

--- aws-node-custom-user-pool-master/postConfirmation.js
+++ aws-node-custom-user-pool-master/postConfirmation.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports.handler = (event, context) => {
-  console.log('User confirmed: User-Pool', event.userPoolId+", UserId: " + event.userName);
+  console.log('User confirmed: User-Pool', event.userPoolId+", UserId: " + event.userName+", event:"+event);
   context.done(null, event);
 }
