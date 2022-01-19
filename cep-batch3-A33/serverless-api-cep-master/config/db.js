79f925880ff273367f3bdfe78bb96d69609644b4
Riddhi Goyal <riddhi.goyal@impetus.com>
2022-01-19 19:07:08 +0530

    Added frontend deployment script

--- cep-batch3-A33/serverless-api-cep-master/config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports =  {
-    database: 'postgres',
-    host: 'database-1.cu02ykjirug4.us-east-1.rds.amazonaws.com',
-    port: '5432',
-    user: 'postgres',
-    password: '12345678',
-}
---
+++ infrastructure/variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+variable "logbuckname" {
+  type = string
+  default = "kloudjetlogsmytftestbucket"
+}
+variable "buckname" {
+  type = string
+  default = "kloudjetmytftestbucket"
+}
