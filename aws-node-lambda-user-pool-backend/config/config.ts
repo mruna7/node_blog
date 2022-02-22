630a4ed694152fdcc1283a3d879c532b1eaea103
vaibhav64 <vg2642119@gmail.com>
2022-02-22 15:26:55 +0530

    test

--- aws-node-lambda-user-pool-backend/config/config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  "dev": {
-    "username": "a72_mysql",
-    "password": "cepa72Batch4",
-    "database": "cepa72",
-    "host": "cep-a72-mysql.cbom4fltpgce.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
-  }
-}
--- aws-node-lambda-user-pool-backend/util/util.ts
+++
@@ -1,5 +0,0 @@
-export const getResponseHeaders = () => {
-    return {
-        'Access-Control-Allow-Origin': '*',
-    }
-}
